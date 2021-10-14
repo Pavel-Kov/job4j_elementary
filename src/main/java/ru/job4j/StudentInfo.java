6d07d8096a39e8a34d54ff6c55ecc7181c2a5f79
Pavel <wangle@bk.ru>
2021-10-14 17:06:57 +0300

    Исправление ошибок после checkstyle

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main(String[] args ) {
+    public static void main(String[] args) {
         System.out.println("Pavel");
         System.out.println("29.12.1984");
     }
