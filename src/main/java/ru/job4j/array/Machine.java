cbfc9336ea431489bb9bda405b71a889b0f5b744
Pavel <wangle@bk.ru>
2021-10-28 10:49:31 +0300

    Исправление 6.9. Жадный алгоритм. Сдача в кофе машине [#34741 #220838]

--- src/main/java/ru/job4j/array/Machine.java
+++ src/main/java/ru/job4j/array/Machine.java
@@ -8,10 +8,10 @@ public class Machine {
         int[] rsl = new int[100];
         int size = 0;
         int dif = money - price;
-        for (int i : coins) {
-            while (dif >= i) {
-                dif -= i;
-                rsl[size] = i;
+        for (int coin : coins) {
+            while (dif >= coin) {
+                dif -= coin;
+                rsl[size] = coin;
                 size++;
             }
 
